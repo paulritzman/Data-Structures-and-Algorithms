1481924924908510779d32ee1d40ee9de4b579dc
paulritzman <ritzman.paul@gmail.com>
2018-06-12 11:28:43 -0700

    Complete unit-tests for methods; Completed application functionality; Completed all linked list methods

--- Challenges/LinkedLists/LinkedLists/Program.cs
+++ Challenges/LinkedLists/LinkedLists/Program.cs
@@ -7,15 +7,145 @@ namespace LinkedLists
     {
         public static void Main(string[] args)
         {
-            
+            string menuSelection = "", newNode = "", existingNode = "";
+
+            LinkedList ll = new LinkedList(new Node("zzz"));
+
+            do
+            {
+                newNode = "";
+
+                PrintMainMenu();
+                menuSelection = Console.ReadLine();
+                Console.Clear();
+
+                switch (menuSelection)
+                {
+                    case "1":
+                        Console.WriteLine("What value you like the new Node to contain?");
+                        newNode = Console.ReadLine();
+
+                        if (newNode != "")
+                        {
+                            ll.AddNode(new Node(newNode));
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Sorry, you must input a value for the Node. Please try again.");
+                        }
+
+                        Console.WriteLine("\nPress any key to return to main menu...");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case "2":
+                        Console.WriteLine("What value you like the new Node to contain?");
+                        newNode = Console.ReadLine();
+                        Console.Clear();
+                        Console.WriteLine("What is the value of the existing Node to insert the new Node before?");
+                        existingNode = Console.ReadLine();
+
+                        if (newNode != "" && existingNode != "")
+                        {
+                            ll.AddBefore(new Node(newNode), new Node(existingNode));
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Sorry, you must input a value for both Nodes. Please try again.");
+                        }
+
+                        Console.WriteLine("\nPress any key to return to main menu...");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case "3":
+                        Console.WriteLine("What value you like the new Node to contain?");
+                        newNode = Console.ReadLine();
+                        Console.Clear();
+                        Console.WriteLine("What is the value of the existing Node to insert the new Node after?");
+                        existingNode = Console.ReadLine();
+
+                        if (newNode != "" && existingNode != "")
+                        {
+                            ll.AddAfter(new Node(newNode), new Node(existingNode));
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Sorry, you must input a value for both Nodes. Please try again.");
+                        }
+
+                        Console.WriteLine("\nPress any key to return to main menu...");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case "4":
+                        Console.WriteLine("What value you like the new Node to contain?");
+                        newNode = Console.ReadLine();
+
+                        if (newNode != "")
+                        {
+                            ll.AddLast(new Node(newNode));
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Sorry, you must input a value for the Node. Please try again.");
+                        }
+
+                        Console.WriteLine("\nPress any key to return to main menu...");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case "5":
+                        Console.WriteLine("What is the value of the Node you would like to find?");
+                        existingNode = Console.ReadLine();
+
+                        if (newNode != "")
+                        {
+                            ll.Find(existingNode);
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Sorry, you must input a value for the Node. Please try again.");
+                        }
+
+                        Console.WriteLine("\nPress any key to return to main menu...");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case "6":
+                        ll.PrintNodes();
+
+                        Console.WriteLine("\nPress any key to return to main menu...");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case "7":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("That did not match one of the menu options. Try again.\n");
+                        break;
+                }
+
+            } while (menuSelection != "7");
         }
 
-        public static void MainMenu()
+        public static void PrintMainMenu()
         {
             Console.WriteLine(
-                    "Select an option from the menu below:" +
+                    "Select an option from the menu below:\n" +
                     "1) Add a Node to the beginning of the Linked List\n" +
-                    "2) Add a Node ");
+                    "2) Add a Node before another Node\n" +
+                    "3) Add a Node after another Node\n" +
+                    "4) Add a Node to the end of the Linked List\n" +
+                    "5) Find a Node\n" +
+                    "6) Print all Nodes\n" +
+                    "7) Exit Program");
         }
     }
 }
--- Challenges/LinkedLists/Unit-Tests/UnitTest1.cs
+++ Challenges/LinkedLists/Unit-Tests/UnitTest1.cs
@@ -9,9 +9,64 @@ namespace Unit_Tests
     public class UnitTest1
     {
         [Fact]
-        public void CanAddNode()
+        public void CanAddNewNode()
         {
             LinkedList ll = new LinkedList(new Node("5"));
+
+            Node node2 = new Node("10");
+            ll.AddNode(node2);
+
+            Assert.Equal(ll.Head.Value, node2.Value);
         }
+
+        [Theory]
+        [InlineData("5", "5")]
+        [InlineData("10", "10")]
+        [InlineData("Hello", "Hello")]
+        [InlineData("World", "World")]
+        [InlineData("!!!", "!!!")]
+        public void CanFindExistingNode(string expected, string value)
+        {
+            LinkedList ll = new LinkedList(new Node("5"));
+
+            Node node2 = new Node("10");
+            Node node3 = new Node("Hello");
+            Node node4 = new Node("World");
+            Node node5 = new Node("!!!");
+            ll.AddNode(node2);
+            ll.AddNode(node3);
+            ll.AddNode(node4);
+            ll.AddNode(node5);
+
+            Node found = ll.Find(value);
+
+            Assert.Equal(expected, found.Value);
+        }
+
+        [Theory]
+        [InlineData("-5")]
+        [InlineData("100")]
+        [InlineData("Hi")]
+        [InlineData("Earth")]
+        [InlineData("...")]
+        public void CanReturnNullForNodeThatDoesNotExist(string value)
+        {
+            LinkedList ll = new LinkedList(new Node("5"));
+
+            Node node2 = new Node("10");
+            Node node3 = new Node("Hello");
+            Node node4 = new Node("World");
+            Node node5 = new Node("!!!");
+            ll.AddNode(node2);
+            ll.AddNode(node3);
+            ll.AddNode(node4);
+            ll.AddNode(node5);
+
+            Node found = ll.Find(value);
+
+            Assert.Null(found);
+        }
+
+
     }
 }
