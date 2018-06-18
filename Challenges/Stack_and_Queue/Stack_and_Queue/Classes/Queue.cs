9333f771bffd9e000590e2ec22c6122e2536f81c
paulritzman <ritzman.paul@gmail.com>
2018-06-18 12:42:29 -0700

    Build initial Class structure

---
+++ Challenges/Stack_and_Queue/Stack_and_Queue/Classes/Queue.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack_and_Queue.Classes
+{
+    class Queue
+    {
+        public Node Front { get; set; }
+        public Node Rear { get; set; }
+
+        public void Enqueue(Node node)
+        {
+            if (Front == null)
+            {
+                Front = node;
+            }
+
+            if (Rear != null)
+            {
+                Rear.Next = node;
+            }
+
+            Rear = node;
+        }
+
+        public Node Dequeue()
+        {
+            if (Front != null)
+            {
+                Node temp = Front;
+                Front = Front.Next;
+
+                temp.Next = null;
+                return temp;
+            }
+
+            return null;
+        }
+
+        public Node Peek()
+        {
+            return Front;
+        }
+    }
+}
---
+++ Challenges/Stack_and_Queue/Stack_and_Queue/Classes/Stack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack_and_Queue.Classes
+{
+    class Stack
+    {
+        public Node Top { get; set; }
+
+        public Stack(Node node)
+        {
+            Top = node;
+        }
+
+        public void Push(Node node)
+        {
+            node.Next = Top;
+            Top = node;
+        }
+
+        public Node Pop()
+        {
+            if (Top != null)
+            {
+                Node Temp = Top;
+                Top = Top.Next;
+
+                Temp.Next = null;
+                return Temp;
+            }
+
+            return null;
+        }
+
+        public Node Peek()
+        {
+            return Top;
+        }
+    }
+}
