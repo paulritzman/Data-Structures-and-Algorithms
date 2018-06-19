ff3efc9a88e992f2137c976c37860c7eaff2741a
paulritzman <ritzman.paul@gmail.com>
2018-06-18 19:44:55 -0700

    Completed code comments

--- DataStructures/Stack_and_Queue/Stack_and_Queue/Classes/Node.cs
+++ DataStructures/Stack_and_Queue/Stack_and_Queue/Classes/Node.cs
@@ -6,9 +6,14 @@ namespace Stack_and_Queue.Classes
 {
     public class Node
     {
+        // Declares fields for the "Next" Node in the Stack or Queue, and the "Value" of each Node
         public string Value { get; set; }
         public Node Next { get; set; }
         
+        /// <summary>
+        /// Constructor method for creating Nodes
+        /// </summary>
+        /// <param name="value">String Value to store in Node</param>
         public Node(string value)
         {
             Value = value;
--- DataStructures/Stack_and_Queue/Stack_and_Queue/Classes/Stack.cs
+++ DataStructures/Stack_and_Queue/Stack_and_Queue/Classes/Stack.cs
@@ -6,19 +6,32 @@ namespace Stack_and_Queue.Classes
 {
     public class Stack
     {
+        // Declares a field for the "Top" (Last-in, First-out) Node in the Stack
         public Node Top { get; set; }
 
+        /// <summary>
+        /// Constructor method for creating the Stack
+        /// </summary>
+        /// <param name="node">Node to add to Stack</param>
         public Stack(Node node)
         {
             Top = node;
         }
 
+        /// <summary>
+        /// Method which adds a Node to the Top of the Stack
+        /// </summary>
+        /// <param name="node">Node to add to Stack</param>
         public void Push(Node node)
         {
             node.Next = Top;
             Top = node;
         }
 
+        /// <summary>
+        /// Method which removes the Top Node from the Stack
+        /// </summary>
+        /// <returns>Node</returns>
         public Node Pop()
         {
             if (Top != null)
@@ -33,6 +46,10 @@ namespace Stack_and_Queue.Classes
             return null;
         }
 
+        /// <summary>
+        /// Method to grab the Top node in the Stack without removing the Node
+        /// </summary>
+        /// <returns>Node</returns>
         public Node Peek()
         {
             return Top;
