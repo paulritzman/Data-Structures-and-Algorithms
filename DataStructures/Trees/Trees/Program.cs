3dc1de51473552b5e359e22dcd61f83fbc8d633c
paulritzman <ritzman.paul@gmail.com>
2018-06-23 19:58:17 -0700

    Created Trees directory; Completed BinaryTree class

---
+++ DataStructures/Trees/Trees/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Trees.Classes;
+
+namespace Trees
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BinaryTree binTree = new BinaryTree(new Node(1));
+
+            for (int i = 2; i < 10; i++)
+            {
+                binTree.Add(binTree.Root, new Node(i));
+            }
+
+            Node foundNode = binTree.Search(binTree.Root, 51);
+
+            if (foundNode != null)
+            {
+                Console.WriteLine(foundNode.Value);
+            }
+            else
+            {
+                Console.WriteLine("That Node doesn't exist in the Binary Tree.");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
