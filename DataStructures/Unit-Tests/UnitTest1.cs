8365082f5e19f7e88deba613f0d15198448db7e5
paulritzman <ritzman.paul@gmail.com>
2018-06-24 23:58:39 -0700

    restructured unit-test directory layout

---
+++ DataStructures/Unit-Tests/UnitTest1.cs
@@ -0,0 +1,57 @@
+using System;
+using Trees.Classes;
+using Xunit;
+
+namespace Unit_Tests
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData(1)]
+        [InlineData(5)]
+        [InlineData(10)]
+        public void CanAddNodeToBinarySearchTree(int inputValue)
+        {
+            Node addedNode = new Node(inputValue);
+            BinarySearchTree binSearchTree = new BinarySearchTree(addedNode);
+
+            Assert.Same(binSearchTree.Root, addedNode);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(20)]
+        [InlineData(50)]
+        public void CanSearchForAndReturnANodeFromBinarySearchTree(int inputValue)
+        {
+            BinarySearchTree binSearchTree = new BinarySearchTree(new Node(15));
+            binSearchTree.Add(binSearchTree.Root, new Node(4));
+            binSearchTree.Add(binSearchTree.Root, new Node(5));
+            binSearchTree.Add(binSearchTree.Root, new Node(17));
+            binSearchTree.Add(binSearchTree.Root, new Node(25));
+            binSearchTree.Add(binSearchTree.Root, new Node(inputValue));
+
+            Node foundNode = binSearchTree.Search(binSearchTree.Root, inputValue);
+
+            Assert.Equal(inputValue, foundNode.Value);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(20)]
+        [InlineData(50)]
+        public void CanSearchForAndReturnNodeFromBinaryTree(int inputValue)
+        {
+            BinaryTree binTree = new BinaryTree(new Node(15));
+            binTree.Add(binTree.Root, new Node(4));
+            binTree.Add(binTree.Root, new Node(5));
+            binTree.Add(binTree.Root, new Node(17));
+            binTree.Add(binTree.Root, new Node(25));
+            binTree.Add(binTree.Root, new Node(inputValue));
+
+            Node foundNode = binTree.Search(binTree.Root, inputValue);
+
+            Assert.Equal(inputValue, foundNode.Value);
+        }
+    }
+}
