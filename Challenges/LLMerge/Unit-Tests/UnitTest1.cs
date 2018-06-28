c37111f3469ff46ec358054f562e0ff2effdf12a
paulritzman <ritzman.paul@gmail.com>
2018-06-27 21:14:09 -0700

    Added unit-test to prove the number of Nodes in one linked lists is increased after merging with another linked list

--- Challenges/LLMerge/Unit-Tests/UnitTest1.cs
+++ Challenges/LLMerge/Unit-Tests/UnitTest1.cs
@@ -35,5 +35,51 @@ namespace Unit_Tests
             Node mergedHead = ll1.MergeLists(ll1, ll2);
             Assert.Equal("11", mergedHead.Next.Value);
         }
+
+        [Fact]
+        public void CanIncreaseTotalLinkListLengthByMergingLists()
+        {
+            // ll1 Nodes are ordered as: 10 --> 5
+            LinkedList ll1 = new LinkedList(new Node("5"));
+            Node node2 = new Node("10");
+            ll1.AddNode(node2);
+
+            // ll2 Nodes are ordered as: 11 --> 6
+            LinkedList ll2 = new LinkedList(new Node("6"));
+            Node node3 = new Node("11");
+            ll2.AddNode(node3);
+
+            // Setup variables for use in while loop
+            int expectedPreMergeCount = 2;
+            int actualPreMergeCount = 0;
+            ll1.Current = ll1.Head;
+
+            // Iterage through Linked List - incrementing actualPreMergeCount to count number of Nodes
+            while (ll1.Current != null)
+            {
+                ll1.Current = ll1.Current.Next;
+                actualPreMergeCount++;
+            }
+
+            Assert.Equal(expectedPreMergeCount, actualPreMergeCount);
+
+            // Merge and recount the number of Nodes
+            ll1.MergeLists(ll1, ll2);
+
+            // Setup variables for use in while loop
+            int expectedPostMergeCount = 4; // 2 + 2
+            int actualPostMergeCount = 0;
+            ll1.Current = ll1.Head;
+
+            // Iterage through Linked List - incrementing actualPostMergeCount to count number of Nodes
+            while (ll1.Current != null)
+            {
+                ll1.Current = ll1.Current.Next;
+                actualPostMergeCount++;
+            }
+
+            Assert.Equal(expectedPostMergeCount, actualPostMergeCount);
+        }
+
     }
 }
